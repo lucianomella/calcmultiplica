5b8c0c019efb8abe0073adbedbb6e11881f61b9e
Luciano Mella <lucianomella@gmail.com>
2020-11-24 23:42:49 -0300

    Se incluye endpint para visualizar coverage

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ let app = express();
 
 app.use(express.json());
 
+app.use( '/coverage',express.static('./coverage/lcov-report/'));
+
 app.use('/multiplica', require('./routes/multi'));
 
 app.listen(PORT);
